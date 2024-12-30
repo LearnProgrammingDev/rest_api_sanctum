fa3dc0357bd224467581086d53977476ddcb5b68
Learn Programming Dev <learnprogramming65@gmail.com>
2024-12-31 01:22:12 +0700

    membuat controller login dengan method invoke, buat route login, membuat test unit login serta method fungsion yang dibutuhka, dan terakhir menjalankannya di postman untuk uji coba

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\Api\LoginController;
 
 Route::post('/register', RegisterController::class);
+Route::post('/login', LoginController::class);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
